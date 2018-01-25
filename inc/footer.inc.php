e30134696e4197b81721e92b620cbf3e950a615e
Ngraphic <35262055+Ngraphic@users.noreply.github.com>
2018-01-25 14:24:20 +0100

    ul li

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,27 +1,20 @@
-	</body>
 		<footer>
-			<div class= "col-xs-10 col-lg-5">
-				<div class="share-buttons">
-					
-		  			
-		  				<a href="https://www.facebook.com/sharer/sharer.php?u=&t=" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="images/flat_web_icon_set/black/Facebook.png" />
-		  				</a>
-		  			
-		  				<a href="https://twitter.com/intent/tweet?source=&text=:%20" target="_blank" title="Tweet"><img alt="Tweet" src="images/flat_web_icon_set/black/Twitter.png" />
-		  				</a>
-		  			
-		  				<a href="https://plus.google.com/share?url=" target="_blank" title="Share on Google+"><img alt="Share on Google+" src="images/flat_web_icon_set/black/Google+.png" />
-		  				</a>
-		  			
-		  				<a href="http://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=&source=" target="_blank" title="Share on LinkedIn"><img alt="Share on LinkedIn" src="images/flat_web_icon_set/black/LinkedIn.png" />
-		  				</a>
-		  			
-				</div>
 
-				
-					Copyright &copy; NH & GF 2017 
-				
+			<div class="col-xs-10 col-lg-8">
+				<ul class="share-buttons ">
+		  			<li><a href="https://www.facebook.com/sharer/sharer.php?u=&t=" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="images/flat_web_icon_set/black/Facebook.png" /></a></li>
+		  			<li><a href="https://twitter.com/intent/tweet?source=&text=:%20" target="_blank" title="Tweet"><img alt="Tweet" src="images/flat_web_icon_set/black/Twitter.png" /></a></li>
+		  			<li><a href="https://plus.google.com/share?url=" target="_blank" title="Share on Google+"><img alt="Share on Google+" src="images/flat_web_icon_set/black/Google+.png" /></a></li>
+		  			<li><a href="http://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=&source=" target="_blank" title="Share on LinkedIn"><img alt="Share on LinkedIn" src="images/flat_web_icon_set/black/LinkedIn.png" /></a></li>
+				</ul>
 			</div>
+			<div>
+
+				Copyright &copy; NH & GF 2017
+
+			</div>
+
+			<!-- </div> -->
 
 			<!-- JavaScript du site -->
 			
@@ -31,5 +24,6 @@
 			<script src="js/bootstrap.min.js"></script>
 
 		</footer>
+	</body>
 
 </html>
--- la_carte.php
+++ la_carte.php
@@ -2,12 +2,45 @@
 include('inc/header.inc.php');
 include('inc/nav.inc.php');
 ?>
+
+
+
 		<section>
-			<div class="conteneur">
+			<div class="container">
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Open modal for @fat</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Menu 23€</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+            <label for="message-text" class="col-form-label">Le gourmet</label>
+            <img src="images/menu_gourmet.jpg" id="menu_gourmet">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 				<h1>Menu</h1>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in venenatis nisi. Aenean vitae tincidunt augue. Proin ante urna, cursus eget ante id, ullamcorper placerat nisi. Quisque in sapien ac turpis accumsan imperdiet. Fusce laoreet pharetra felis, ac efficitur nisi molestie porttitor. Pellentesque vitae justo neque. Donec congue nulla arcu, eu sodales lorem ullamcorper suscipit. Integer gravida suscipit neque quis fringilla. Etiam facilisis justo vitae lacus viverra posuere eget quis ante. Nulla accumsan urna bibendum vestibulum pretium. Donec aliquet velit a nisl eleifend faucibus.
 			</div>
 		</section>
+
 		
 <?php
 include('inc/footer.inc.php');
