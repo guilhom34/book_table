efb60c83edbacbf517f06c90abe388135862c4c8
Ngraphic <35262055+Ngraphic@users.noreply.github.com>
2018-02-04 21:38:15 +0100

    social network shares
    centrés dans le footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,25 +1,23 @@
 		<footer>
 
 			<div class="col-xs-10 col-lg-8">
-				<div><ul class="share-buttons ">
-		  			<div><li>
+				<div class="share-buttons" style="display: block; text-align: center;">
+		  			
 		  				<a href="https://www.facebook.com/sharer/sharer.php?u=&t=" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="images/flat_web_icon_set/black/Facebook.png" /></a>
-		  			</li></div>
-		  			<div><li>
+
 		  				<a href="https://twitter.com/intent/tweet?source=&text=:%20" target="_blank" title="Tweet"><img alt="Tweet" src="images/flat_web_icon_set/black/Twitter.png" /></a>
-		  			</li></div>
-		  			<div><li>
+	
 		  				<a href="https://plus.google.com/share?url=" target="_blank" title="Share on Google+"><img alt="Share on Google+" src="images/flat_web_icon_set/black/Google+.png" /></a>
-		  			</li></div>
-		  			<div><li>
+	
 		  				<a href="http://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=&source=" target="_blank" title="Share on LinkedIn"><img alt="Share on LinkedIn" src="images/flat_web_icon_set/black/LinkedIn.png" /></a>
-		  			</li></div>
-				</ul>
 
-			</div>
+		  		<div>
+		  			Copyright &copy; NH & GF 2017
+		  		</div>
+
+		  		</div>
 		
-				Copyright &copy; NH & GF 2017
-
+				
 			</div>
 
 			<!-- </div> -->
